e89f1c1c4cce5d3c9b023c7cc45a19499cc67473
emil <emilbabazade29@gmail.com>
2020-12-03 14:16:56 +0200

    changed navigation classes to navigationdirection classes

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -34,8 +35,11 @@ class GameWonFragment : Fragment() {
                 inflater, R.layout.fragment_game_won, container, false)
 
         binding.nextMatchButton.setOnClickListener {
-            it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment2)
+            it.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment2())
         }
+
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "${args.numCorrect} out of ${args.numQuestions} correct", Toast.LENGTH_LONG).show()
 
         return binding.root
     }
